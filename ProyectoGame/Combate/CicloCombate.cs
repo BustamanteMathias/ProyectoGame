857c69e266b6a27395d40f9c0edcad3bc34a1e2e
axelghio <36314606+axelghio@users.noreply.github.com>
2019-09-22 18:24:34 -0300

    ciclo maste

---
+++ ProyectoGame/Combate/CicloCombate.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1.Combate
+{
+    public static class CicloCombate
+    {
+        public static bool check = true;
+
+        public static void checkear()
+        {
+            if (check == true)
+            {
+                Console.Write("PUTITO");
+                Console.Write("Gato");
+            }
+        }
+    }
+}
